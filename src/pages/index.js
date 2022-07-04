c5bb2a51492371655eb2b0f4b36f0fb407103c43
Peter Chow <selllfocard@gmail.com>
2022-07-04 11:22:22 +0900

    update example

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,23 @@ import { useTranslation } from 'react-i18next';
 export default function Home() {
   const { t } = useTranslation();
   return (
-    <div>
+    <div className='p-4 border border-black inline-block rounded m-4'>
       <div>{`Home, ${t('title')}`}</div>
       <div>
-        <a href='/?lng=zh'>View zh-lang</a>
+        <a className='underline' href='/?lng=zh'>
+          View zh-lang
+        </a>
         <br />
-        <a href='/?lng=en'>View en-lang</a>
+        <a className='underline' href='/?lng=en'>
+          View en-lang
+        </a>
+        <br />
+        <a className='underline' href='/api/hello'>
+          API: hello
+        </a>
+      </div>
+      <div>
+        
       </div>
     </div>
   );
