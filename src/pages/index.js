7503e7ad07484404392f0dc4776759ac2aa4a59f
Peter Chow <selllfocard@gmail.com>
2022-07-05 10:13:31 +0900

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,37 @@
 export default function Home() {
+  const links = [
+    {
+      name: 'Gtihub',
+      emoji: '🐱',
+      url: 'https://github.com/peteroid',
+    },
+    {
+      name: 'Linkedin',
+      emoji: '💼',
+      url: 'https://linkedin.com/in/peteroid',
+    },
+    {
+      name: 'Résumé',
+      emoji: '📇',
+      url: 'https://drive.google.com/file/d/1acKX5RhWBtU51rprALCkctZ8ihFIkYcO/view?usp=sharing',
+    },
+  ];
+
   return (
-    <div className='h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col justify-center items-center text-white gap-8'>
-      <h2 className='text-xl'>Hello world!</h2>
-      <h1 className='text-xl'>
-        This is <span className='italic'>Peter Chow</span> <span className='text-4xl'>🐕</span>
-      </h1>
-      <div className='flex flex-col text-center gap-2'>
-        <a className='underline' href='https://github.com/peteroid'>
-          Github
-        </a>
-        <a className='underline' href='https://linkedin.com/in/peteroid'>
-          Linkedin
-        </a>
-        <a
-          className='underline'
-          href='https://drive.google.com/file/d/1acKX5RhWBtU51rprALCkctZ8ihFIkYcO/view?usp=sharing'
-        >
-          CV
-        </a>
+    <div className='h-full bg-gradient-to-br from-cyan-500 via-sky-500 to-indigo-500 flex flex-col justify-center items-center'>
+      <div className='bg-white rounded-2xl shadow-2xl flex flex-col justify-center items-center gap-4 p-8 md:p-16 md:gap-8'>
+        <h1 className='text-xl flex items-center gap-2'>
+          <span className='italic'>Peter Chow</span>{' '}
+          <span className='text-3xl animate-bounce'>🐕</span>
+        </h1>
+        <div className='flex flex-col text-center gap-2'>
+          {links.map((link) => (
+            <a key={link.name} href={link.url} target='_blank' rel='noreferrer'>
+              <span className='underline'>{link.name}</span>
+              <span className='ml-2 text-xl'>{link.emoji}</span>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
