31828ad20534c5a9b6c036a7425d4225c5136ecf
Peter Chow <selllfocard@gmail.com>
2022-07-06 10:51:25 +0900

    update package and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: { raw: 'print, (min-width: 640px)' },
+      },
+    },
   },
   variants: {
     extend: {},
